392db2f2b4684def1a55650fa35578607b6d6c97
Grant Phelps <gbphelps2@gmail.com>
2021-06-06 12:13:15 -0400

    Broken resize observer refactor

--- javascripts/resizeEvents.js
+++ javascripts/resizeEvents.js
@@ -1,9 +1,58 @@
-const MARGIN = 20;
+import ResizeObserver from 'resize-observer-polyfill';
+
+function resizeColorEditor(parentRect) {
+  const minRatio = 1.6;
+  const maxRatio = 1.8;
+
+  let height;
+  let width;
+
+  if (parentRect.width / parentRect.height > maxRatio) {
+    height = parentRect.height;
+    width = parentRect.height * maxRatio;
+  } else if (parentRect.width / parentRect.height < minRatio) {
+    width = parentRect.width;
+    height = parentRect.width / minRatio;
+  } else {
+    height = parentRect.height;
+    width = parentRect.width;
+  }
+  return {
+    height,
+    width,
+  };
+}
+
+function resizeBlockSliders(parentRect) {
+  return {
+    height: parentRect.height * 0.4,
+  };
+}
+
+function resizeTop(parentRect) {
+  return {
+    height: parentRect.height * 0.6,
+  };
+}
 
 class ResizeEvents {
   constructor() {
     this.subscriptions = [];
-    window.addEventListener('resize', this.resize.bind(this));
+    // window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  addResizeObserver(el, child, func) {
+    func(el.getBoundingClientRect());
+    const ro = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        const dims = func(entry.contentRect);
+        Object.assign(child.style, {
+          width: dims.width != null ? `${dims.width}px` : 'unset',
+          height: dims.height != null ? `${dims.height}px` : 'unset',
+        });
+      });
+    });
+    ro.observe(el);
   }
 
   subscribe(fn) {
@@ -12,24 +61,35 @@ class ResizeEvents {
   }
 
   init() {
-    this.resize();
+    this.addResizeObserver(
+      document.getElementById('ooo'),
+      document.getElementById('color-editor-container'),
+      resizeColorEditor,
+    );
+    this.addResizeObserver(
+      document.getElementById('color-editor-container'),
+      document.getElementById('block-sliders'),
+      resizeBlockSliders,
+    );
+    this.addResizeObserver(
+      document.getElementById('color-editor-container'),
+      document.getElementById('top'),
+      resizeTop,
+    );
   }
 
   // eslint-disable-next-line class-methods-use-this
   resize() {
     const ooo = document.getElementById('ooo');
-    const iii = document.getElementById('iii');
-    const bs = document.getElementById('block-sliders');
-    const top = document.getElementById('top');
-    const main = document.getElementById('main');
-    const hsl = document.getElementById('hsl');
-    const hsv = document.getElementById('hsv');
-    const lb = document.getElementById('lightness-blocks');
-    const rc = document.getElementById('right-container');
+    // const main = document.getElementById('main');
+    // const hsl = document.getElementById('hsl');
+    // const hsv = document.getElementById('hsv');
+    // const lb = document.getElementById('lightness-blocks');
+    // const rc = document.getElementById('right-container');
 
-    const rgbcmyk = document.getElementById('rgb-cmyk');
-    const rgb = document.getElementById('rgb');
-    const cmyk = document.getElementById('cmyk');
+    // const rgbcmyk = document.getElementById('rgb-cmyk');
+    // const rgb = document.getElementById('rgb');
+    // const cmyk = document.getElementById('cmyk');
 
     const minRatio = 1.6;
     const maxRatio = 1.8;
@@ -52,34 +112,35 @@ class ResizeEvents {
       container.height = rect.height;
       container.width = rect.width;
     }
-    iii.style.height = `${container.height}px`;
-    iii.style.width = `${container.width}px`;
 
-    const topHeight = (container.height - MARGIN) * 0.6;
-    main.style.height = `${topHeight}px`;
-    main.style.width = `${topHeight}px`;
-    top.style.height = `${topHeight}px`;
-    bs.style.height = `${(container.height - MARGIN) * 0.4}px`;
+    // iii.style.height = `${container.height}px`;
+    // iii.style.width = `${container.width}px`;
 
-    const numBlocks = 21;
-    const blockMargin = 1;
+    // const topHeight = (container.height - MARGIN) * 0.6;
+    // main.style.height = `${topHeight}px`;
+    // main.style.width = `${topHeight}px`;
+    // top.style.height = `${topHeight}px`;
+    // bs.style.height = `${(container.height - MARGIN) * 0.4}px`;
 
-    const lbWidth = (
-      container.height - 2 * MARGIN - (numBlocks - 1) * blockMargin
-    ) / numBlocks * 2 + 3 * MARGIN;
+    // const numBlocks = 21;
+    // const blockMargin = 1;
 
-    lb.style.width = `${lbWidth}px`;
-    const rightWidth = container.width - MARGIN - lbWidth;
+    // const lbWidth = (
+    //   container.height - 2 * MARGIN - (numBlocks - 1) * blockMargin
+    // ) / numBlocks * 2 + 3 * MARGIN;
 
-    rc.style.width = `${rightWidth}px`;
-    hsl.style.width = `${(rightWidth - MARGIN)}px`;
-    hsv.style.width = `${(rightWidth - MARGIN) * 0.35}px`;
+    // lb.style.width = `${lbWidth}px`;
+    // const rightWidth = container.width - MARGIN - lbWidth;
 
-    rgbcmyk.style.width = `${rightWidth - MARGIN - topHeight}px`;
-    rgbcmyk.style.height = `${topHeight}px`;
+    // rc.style.width = `${rightWidth}px`;
+    // hsl.style.width = `${(rightWidth - MARGIN)}px`;
+    // hsv.style.width = `${(rightWidth - MARGIN) * 0.35}px`;
 
-    rgb.style.height = `${(topHeight - MARGIN) * 0.43}`;
-    cmyk.style.height = `${(topHeight - MARGIN) * 0.57}`;
+    // rgbcmyk.style.width = `${rightWidth - MARGIN - topHeight}px`;
+    // rgbcmyk.style.height = `${topHeight}px`;
+
+    // rgb.style.height = `${(topHeight - MARGIN) * 0.43}`;
+    // cmyk.style.height = `${(topHeight - MARGIN) * 0.57}`;
   }
 }
 
